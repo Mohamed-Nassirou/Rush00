e97fa8e474243d33bf121eeade3b87500bbfdd8c
mohamednassirouahamadou9696@gmail.com <mohamednassirouahamadou9696@gmail.com>
2023-02-12 22:16:06 +0100

    Rush00

---
+++ numberToString.js
@@ -0,0 +1,8 @@
+function numberToString() {
+  var Entrer = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, );
+  console.log(Entrer);
+  var Sortie = new Array("un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix");
+  console.log(Sortie);
+}
+numberToString();
+module.exports = numberToString ;
